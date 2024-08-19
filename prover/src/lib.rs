8dec862fbb3cb5bc98856dafade4486c7355d42b
GopherJ <alex_cj96@foxmail.com>
2024-08-19 13:10:24 +0800

    test
    Signed-off-by: GopherJ <alex_cj96@foxmail.com>

--- prover/src/lib.rs
+++ prover/src/lib.rs
@@ -178,7 +178,7 @@ pub trait Prover {
     /// Returns a tuple containing a [TracePolyTable] with the trace polynomials for the main trace
     /// and a new [TraceLde] instance from which the LDE and trace commitments can be obtained.
     async fn new_trace_lde<'a, E>(
-        &self,
+        &'a self,
         trace_info: &'a TraceInfo,
         main_trace: &'a ColMatrix<Self::BaseField>,
         domain: &'a StarkDomain<Self::BaseField>,
